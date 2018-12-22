8825834c12806dc4a7e0020bcc12385768363b04
Raymond Chow <raychow1944@yahoo.com>
2018-12-22 02:10:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,10 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season.to_s.capitalize!}:"
     holidays.each do |holiday, supplies|
-      
+      str = ""
+      holiday.split(' ').each do |word| 
+        word.capitalize!
+      end
     end
   end
 end
