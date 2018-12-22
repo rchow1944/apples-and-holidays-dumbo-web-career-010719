708035ff10c04a61d6b64a0a802cb60a13976a37
Raymond Chow <raychow1944@yahoo.com>
2018-12-22 02:08:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,9 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holidays|
     puts "#{season.to_s.capitalize!}:"
-    
+    holidays.each do |holiday, supplies|
+      
+    end
   end
 end
 
